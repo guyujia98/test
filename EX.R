7461583c3220b50df8096695d4f68afe70e86bcd
guyujia98 <guyujia98@163.com>
2020-02-05 12:31:42 +0800

    One Exercise

---
+++ EX.R
@@ -0,0 +1,37 @@
+N = 100
+a = sample(1:100, 100, replace=T)
+b = sample(1:100, 100, replace=T)
+A = matrix(nrow = 100, ncol = 100)
+B = matrix(nrow = 100, ncol = 100)
+sumA = c()
+probA = c()
+error = c()
+errorT=c()
+upbound = c()
+
+for (i in 1:N)
+{
+	A[i,] = sample(1:a[i], 100, replace=T)
+	B[,i] = sample(1:b[i], 100, replace=T)
+	sumA[i] = sum(A[,i]^2)
+	probA[i] = sumA[i]/sum(sumA) 
+}		
+for (i in 1:N)
+{
+	for(k in 1:N)
+	{
+		colAsp = sample(1:100, i, replace=T, prob=probA)
+		Asp = matrix(nrow=100,ncol=i)
+		Bsp = matrix(nrow=i,ncol=100)
+		for (j in 1:i)
+		{
+			Asp[,j] = A[,colAsp[j]]/sqrt(i*probA[colAsp[j]])
+			Bsp[j,] = B[colAsp[j],]/sqrt(i*probA[colAsp[j]])
+		}
+		errorT[k] = norm(A%*%B-Asp%*%Bsp,type="2")
+		error[i] = mean(errorT)
+	}
+}
+		 
+		
+	
